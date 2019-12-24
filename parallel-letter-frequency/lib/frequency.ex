69b3a66a0ddd9bd3f95ab1553b5e7cbab3579f36
togmund <togmund@gmail.com>
2019-12-23 19:05:49 -0800

    scaffolding out graphemes and counts

--- parallel-letter-frequency/lib/frequency.ex
+++ parallel-letter-frequency/lib/frequency.ex
@@ -8,5 +8,28 @@ defmodule Frequency do
   """
   @spec frequency([String.t()], pos_integer) :: map
   def frequency(texts, workers) do
+    # ["ab -c", "A,B.C"] => %{"a" => 2, "b" => 2, "c" => 2}
+    # ["a", "b"," ", ...]
+    texts
+    |> extract_all_graphemes()
+    |> count_letters()
+  end
+
+  defp extract_all_graphemes(texts) do
+    texts
+    |> Enum.join()
+    |> String.graphemes()
+  end
+
+  defp count_letters(graphemes) do
+    Eunum.reduce(graphemes, %{}, fn grapheme, acc ->
+      nil
+      # if grapheme is a letter
+      # downcase
+      # increment count of letter in acc
+
+      # else
+      # nothing
+    end)
   end
 end
